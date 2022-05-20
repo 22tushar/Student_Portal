4dd4af92a4836f40e0a2c93b338d293ed2f7c7d9
22tushar <gehlottushar18@gmal.com>
2022-05-20 22:56:11 +0530

    add all files

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useContext, useRef, useState } from "react";
+import { Link } from "react-router-dom"
+import { Context } from "../../context/Context";
+import "./login.css"
+import loginimage from '../../images/loginimage.png'
+// import { SignInWithGoogle } from "../../firebase";
+export default function Login() {
+    const userRef= useRef()
+    const passwordRef = useRef()
+    const { dispatch, isFetching} = useContext(Context)
+
+    const [error, setError] = useState(false)
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        setError(false)
+        dispatch({type:"LOGIN_START"});
+        
+        await axios.post("/auth/login",{
+            username:userRef.current.value,
+            password:passwordRef.current.value,
+        })
+        .then((res)=>{
+            dispatch({type:"LOGIN_SUCCESS", payload:res.data});
+        })
+        .catch((err)=>{
+            dispatch({type:"LOGIN_FAILURE"});
+            setError(true)
+        })
+    }
+
+    
+    
+    return (
+        <div className="login">
+
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col">
+                        
+                        <form className="loginForm" onSubmit={handleSubmit}>
+                        <span className="loginTitle">Login</span>
+                            <label>Organisation</label>
+                            <input type="text" className="loginInput" placeholder="Enter your organisation..." 
+                            ref={userRef} autoFocus/>
+
+                            <label>Password</label>
+                            <input type="password" className="loginInput" placeholder="Enter your password..."
+                            ref={passwordRef} />
+
+                            <button className="loginButton" type="submit" disabled={isFetching}>Login</button>
+                            <button className="loginButton" type="submit" disabled={isFetching}>Login With Google</button>
+                        </form>
+                       
+                        {error? (<span style={{color:"red", marginTop:"15px"}}> Something went wrong! </span>):(null) }
+
+                    </div>
+     
+                </div>
+            </div>
+
+        </div>
+    )
+}
