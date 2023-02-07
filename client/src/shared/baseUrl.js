064581959b333119d331ee1de4929c220bcf4a13
Tushar Gehlot <86909566+22tushar@users.noreply.github.com>
2023-02-07 18:07:22 +0530

    add url

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseURL = 'https://studentportal02.herokuapp.com/api';
+export const baseURL = 'https://student-portal-skfr.vercel.app/';
 
 export const publicFile = 'https://studentportal02.herokuapp.com/api/images/';
